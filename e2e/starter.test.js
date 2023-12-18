9bf4597baef7ee37353258bbaf391fc7af3b9e13
Vladyslav <kyrychenko.vladyslav1998@gmail.com>
2023-12-18 18:52:11 +0100

    new e2e test for ios

---
+++ e2e/starter.test.js
@@ -0,0 +1,20 @@
+/* global device, element, by, expect, describe, beforeEach, it, beforeAll */
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  it('should show hello screen after tap', async () => {
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.text('Hello, World!'))).toBeVisible();
+  });
+});
